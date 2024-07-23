75316452905bdcc3e14f252e01cb65e5b84af6d9
rickyrod4 <dj3d@Ricardos-Air.lan>
2024-07-23 14:03:31 -0500

    Practice 7.23

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
 
 
+
+
 public class Main{
     public static void main(String[] args) {
         
+        String name = "Ricky      ";
 
-
-
-
-
-
-
+        //boolean result = name.equalsIgnoreCase("ricky");
+        //int result = name.length();
+        //char result = name.charAt(2);
         
+        //int result = name.indexOf('c');
+        //boolean result = name.isEmpty();
+        String result = name.replace('i', 'o');
+        
+        System.out.println(result);
+
+
     }
 }
